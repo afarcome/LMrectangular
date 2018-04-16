12c43eae8e2fb2d5872a07c0d55e4011f79aab29
afarcome <alessio.farcomeni@uniroma1.it>
2018-04-16 16:03:43 +0200

    removed sensitivity Cmax, added dataAnalysis

--- dataAnalysis/dataAnalysis2.r
+++ dataAnalysis/dataAnalysis2.r
@@ -8,21 +8,25 @@ u=unique(HDIdata[,1])
 for(j in 1:6) {
 for(h in 1:3) {
 y[,j,h]=HDIdata[HDIdata[,1]==u[j],h+2]}}
+#y[,,1]=log(y[,,1])
 
 source("codeKvariable.r")
 
 x=c(0,0.02,0.05,0.07,0.1,0.15,0.2,0.25,0.3,0.35,0.375,0.4,0.425,0.45,0.5)
 
 set.seed(1234)
-rl0=rlm(y,0,4,cmax=2)
+rl0=rlm(y,0,4)
 rl=list()
 l=length(x)
 rl[[1]]=rl0
 for(j in 2:l) {
-rl[[j]]=rlm(y,x[j],4,inits=rl[[j-1]],cmax=2)}
+rl[[j]]=rlm(y,x[j],4,inits=rl[[j-1]])}
 
 liks=sapply(1:l,function(j) rl[[j]]$lik)
 crit=diff(liks)/(mean(liks)*diff(x))
 
+pdf("scree2.pdf")
+plot(x,liks,type="l",xlab="lambda",ylab="pLik")
+dev.off()
 
-save.image(file="rl.rda")
+save.image(file="rl2.rda")
---
+++ dataAnalysis/ncpu.r
@@ -0,0 +1,5 @@
+ncpu=15
+
+ncpu=max(ncpu,2)
+ncpu=min(ncpu,15)
+
