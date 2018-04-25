cc86429565bb6d9e2a51c3ffabdcb8e90ac01255
afarcome <alessio.farcomeni@uniroma1.it>
2018-04-25 07:12:11 +0200

    modified:   dataAnalysis/sensitivityB.r

--- dataAnalysis/sensitivityB.r
+++ dataAnalysis/sensitivityB.r
@@ -19,19 +19,9 @@ set.seed(12345)
 load("rl.rda")
 Bs=c(10,50,100,200,500)
 rlB=list()
-inits=rl[[7]]
-inits$k=rep(3,6)
-iter=1
-for(B in Bs) {
-    rlB[[iter]]=rlm(y,0.3,4,inits=inits,hits=50,B=B)
-iter=iter+1
-}
 
-rlB2=list()
-inits$k=rep(4,6)
-iter=1
-for(B in Bs) {
-    rlB2[[iter]]=rlm(y,0.3,4,inits=inits,hits=50,B=B)
+for(iter in 1:5) {
+    rlB[[iter]]=rlm(y,0,4,inits=inits0,hits=50,B=Bs[iter])
 iter=iter+1
 }
 
